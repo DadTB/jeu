a29ab515f3b701b3c0108b8460d743436fda4dfb
Bernard <thomas.bernard45@orange.fr>
2024-11-17 16:00:27 +0100

    aa

---
+++ window.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <windows.h>
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+
+class Window{
+    private:
+
+    HINSTANCE m_hInstance;
+    HWND m_hWnd;
+
+    public:
+    Window();
+    Window(const Window&) = delete; // permet de supprimer les constructeurs par copie
+    Window& operator =(const Window&) = delete;
+    ~Window();
+
+    bool processmessages();
+
+};
+
+class patate{
+
+    public:
+
+    void salut();
+
+};
