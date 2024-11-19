a785d8e38d0a5a5440b22d0f1bd4378bcaeaf782
Bernard <thomas.bernard45@orange.fr>
2024-11-19 13:04:02 +0100

    a

--- classeperso.h
+++ classeperso.h
@@ -1,3 +1,31 @@
-class Barbare{
+
+
+class ClasseBarbare{
+
+};
+
+class ClasseGuerrier{
+
+};
+
+// les classes suivantes sont pour les utilisateurs de la magie
+
+class ClasseMagicien{  // ClasseMagicien est classe mère de toutes les autres classes de magicien
+
+};
+
+class ClasseMageBlanc : public ClasseMagicien{
+
+};
+
+class ClasseMageNoir : public ClasseMagicien{ // ClasseMageNoir est classe mère de : ClasseNecromancien
+
+};
+
+class ClasseNecromancien : public ClasseMageNoir{
+
+};
+
+class ClasseDemonologiste : public ClasseMageNoir{
 
 };
