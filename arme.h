b1c79fa6d4ce76b3127248b1137aa3ab536104a1
Bernard <thomas.bernard45@orange.fr>
2024-11-19 13:30:34 +0100

    a

--- arme.h
+++ arme.h
@@ -1,16 +1,25 @@
 #ifndef ARME_H
 #define ARME_H
 
+// cette partie est pour les armes de melee
 
 class armemelee{
 
 };
+
+// cette partie est pour les amres à distance
 
 class armedistance{
 
 };
 
 class epee : public armemelee{
+
+};
+
+// cette partie est pour les batons de magiciens
+
+class baton{
 
 };
 
