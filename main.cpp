ac0e2fb74456e3d5f7d6e9db35d4343628095c5f
Bernard <thomas.bernard45@orange.fr>
2024-12-12 15:47:15 +0100

    aa

--- main.cpp
+++ main.cpp
@@ -1,73 +1,15 @@
-#include <fstream>
-#include <string>
-#include <iostream>
-#include <stdio.h>
-#include <SFML/Graphics.hpp>
-#include "Terrain/generationterrain.cpp"
-#include "deplacement/deplacement.cpp"
-#include "Inventaire/inventaire.cpp"
+#include "jeu.hpp"
 
 using namespace std;
 
-bool estdansinven = false;
+
 
 
 int main()
 {
-    Deplacement d0;
-    Inventaire i0(0.f, 0.f, 1800.f, 1000.f);
-    GenerationTerrain g0;
+    Jeu j0;
 
-    sf::Clock clock;
-
-    sf::RenderWindow window(sf::VideoMode(1800, 1000), "Donjon et dragons");
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::I)
-            {
-                i0.toggle(); // Ouvrir ou fermer l'inventaire
-                if (estdansinven == false)
-                {
-                    estdansinven = true;
-                }
-                else
-                {
-                    estdansinven = false;
-                }
-            }
-        }
-
-        // Détecter l'appui sur la touche I pour ouvrir ou fermer l'inventaire
-
-        // Gérer les entrées de direction
-        if (estdansinven == false)
-        {
-            d0.gereinput();
-        }
-
-        // Temps écoulé depuis la dernière itération
-        sf::Time deltaTime = clock.restart();
-
-        // Mettre à jour la position du personnage
-        d0.mettreajour(deltaTime, g0.walls);
-
-        // Rendu du terrain
-        g0.renderGrid(window);
-
-        i0.draw(window);
-
-        // Affichage du sprite du personnage
-        window.draw(d0.getRectangle());
-
-        // Affichage à l'écran
-        window.display();
-    }
+    j0.DaggerFall2();
 
     return 0;
 }
